35e6594fd14d6fb117f66a6c6f6754522413992e
vikassandhu999 <vikassandhu999@gmail.com>
2021-08-01 23:55:58 +0530

    refac: extract method

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -24,52 +24,53 @@ export class GildedRose {
     private readonly _sulfuras = 'Sulfuras';
 
     updateQuality(): Array<Item> {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != this._agedBrie && this.items[i].name != this._backstagePasses) {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != this._sulfuras) {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == this._backstagePasses) {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
+        this.items.forEach((item) => this.updateItemQuality(item));
+        return this.items;
+    }
+
+    private updateItemQuality(item: Item) {
+        if (item.name != this._agedBrie && item.name != this._backstagePasses) {
+            if (item.quality > 0) {
+                if (item.name != this._sulfuras) {
+                    item.quality = item.quality - 1
                 }
             }
-            if (this.items[i].name != this._sulfuras) {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != this._agedBrie) {
-                    if (this.items[i].name != this._backstagePasses) {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != this._sulfuras) {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+                if (item.name == this._backstagePasses) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
                         }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
                     }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
                     }
                 }
             }
         }
-
-        return this.items;
+        if (item.name != this._sulfuras) {
+            item.sellIn = item.sellIn - 1;
+        }
+        if (item.sellIn < 0) {
+            if (item.name != this._agedBrie) {
+                if (item.name != this._backstagePasses) {
+                    if (item.quality > 0) {
+                        if (item.name != this._sulfuras) {
+                            item.quality = item.quality - 1
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+        }
     }
 }
